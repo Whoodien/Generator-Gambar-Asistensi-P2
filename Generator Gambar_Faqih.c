1ec8f58a2e9fb053dc047737bdffd6337d7f743c
Whoodien <faqihherali0@gmail.com>
2024-10-21 11:59:46 +0700

    Add files via upload

---
+++ Generator Gambar_Faqih.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> // Untuk fungsi sleep
+#include <time.h>
+
+#define MIN_LENGTH 10
+#define MIN_WIDTH 5
+#define MAX_LENGTH 100
+#define MAX_WIDTH 75
+
+void generateStaticImage(int length, int width) {
+    srand(time(NULL));
+    // Menampilkan gambar statis
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < width; j++) {
+            int random_choice = rand() % 2;
+            if (random_choice == 0){
+                printf("*");
+            } else{
+            printf(" ");}
+        }
+        printf("\n");
+    }
+}
+
+void generateMovingImage(int length, int width) {
+    // Menampilkan gambar yang bergerak
+    for (int offset = 0; offset < width; offset++) {
+        // Menampilkan gambar dengan offset
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                if (j < offset || j >= offset + width) {
+                    printf(" ");
+                } else {
+                    printf("*");
+                }
+            }
+            printf("\n");
+        }
+        usleep(100000); // Delay 100 ms
+    }
+}
+
+int main() {
+    int length, width;
+    char choice;
+
+    while (1) {
+        printf("Masukkan panjang gambar (10-100): ");
+        scanf("%d", &length);
+        printf("Masukkan lebar gambar (5-75): ");
+        scanf("%d", &width);
+
+        // Validasi input
+        if (length < MIN_LENGTH || length > MAX_LENGTH) {
+            printf("Panjang harus antara %d dan %d.\n", MIN_LENGTH, MAX_LENGTH);
+            continue;
+        }
+        if (width < MIN_WIDTH || width > MAX_WIDTH) {
+            printf("Lebar harus antara %d dan %d.\n", MIN_WIDTH, MAX_WIDTH);
+            continue;
+        }
+
+        // Memilih mode tampilan
+        printf("Pilih mode tampilan (s untuk statis, m untuk bergerak): ");
+        scanf(" %c", &choice); // Spasi sebelum %c untuk mengabaikan newline
+
+        if (choice == 's') {
+            generateStaticImage(length, width);
+        } else if (choice == 'm') {
+            generateMovingImage(length, width);
+        } else {
+            printf("Pilihan tidak valid. Silakan pilih 's' atau 'm'.\n");
+        }
+    }
+
+    return 0;
+}
